1c92840d239284b82865df760e1fc10147b52f24
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-13 13:37:44 +0000

    Answer

--- PP1.py
+++ PP1.py
@@ -1,16 +1,16 @@
 def q1():
   #Write Assignment code here
-print("Hello world")
+  print("Hello world")
 def q2():
   #Write Assignment code here
-print(1)
-print(2)
-print(3)
-print(4)
-print(5)
+  print(1)
+  print(2)
+  print(3)
+  print(4)
+  print(5)
 def q3():
   #Write Assignment code here
   print(" Help Me!")
 def q4:
   #Write Assignment code here
-print("3 x 3 = " , 3*3)
+  print("3 x 3 = " , 3*3)
